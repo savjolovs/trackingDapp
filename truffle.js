3630620b9062875269729a40a5592b17254f9ad8
Vadims Savjolovs <savjolovs@gmail.com>
2021-12-29 10:20:42 +0000

    commit

--- truffle.js
+++ truffle.js
@@ -1,10 +1,27 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "4a4b8e1e22724cf1ae4e6915cb018485";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    
   },
 
   compilers: {
